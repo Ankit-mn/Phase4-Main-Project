92b9bd3f1c6739dc11433b23432090307a56e200
Ankit-mn <83132287+Ankit-mn@users.noreply.github.com>
2021-10-24 13:06:38 +0530

    Add files via upload

---
+++ js/quiz.js
@@ -0,0 +1,115 @@
+let quiz=[
+    {
+        question:	
+        "Suppose that you would like to create an instance of a new Map that has an iteration order that is the same as the iteration order of an existing instance of a Map. Which concrete implementation of the Map interface should be used for the new instance?",
+        option:[
+            "1.TreeMap",
+            "2.HashMap",
+            "3.LinkedHashMap",
+            "4.The answer depends on the implementation of the existing instance.",
+        ],
+        answer:3,
+    },
+    {
+        question:" Which class does not override the equals() and hashCode() methods, inheriting them directly from class Object?",
+        option:[
+            "1.java.lang.String",
+            "2.java.lang.Double",
+            "3.	java.lang.StringBuffer",
+            "4.	java.lang.Character",
+            
+        ],
+        answer:3,
+    },
+    {
+        question:"In JavaScript the x===y statement implies that:",
+        option:[
+            "1.Both x and y are equal in value, type and reference address as well.",
+            "2.Both are x and y are equal in value only.",
+            "3.Both are equal in the value and data type.",
+           "4.Both are not same at all.",   
+        ],
+        answer:3,
+    },
+    {
+        question:"Which collection class allows you to grow or shrink its size and provides indexed access to its elements, but whose methods are not synchronized?",
+        option:[
+            "1.java.util.HashSet",
+            "2.java.util.LinkedHashSet ",
+            "3.java.util.List",
+            "4.java.util.ArrayList",   
+        ],
+        answer:4,
+
+    },
+    {
+        question:"You need to store elements in a collection that guarantees that no duplicates are stored and all elements can be accessed in natural order. Which interface provides that capability?",
+        option:[
+            "1.java.util.Map",
+            "2.java.util.Set",
+            "3.	java.util.List",
+            "4. java.util.Collection",   
+        ],
+        answer:2,
+    },
+    {
+        question:"Which collection class allows you to grow or shrink its size and provides indexed access to its elements, but whose methods are not synchronized?",
+        option:[
+            "1.java.util.HashSet",
+            "2.java.util.LinkedHashSet",
+            "3.java.util.List",
+            "4.java.util.ArrayList",   
+        ],
+        answer:4,
+    },
+
+{
+        question:	
+        "You need to store elements in a collection that guarantees that no duplicates are stored and all elements can be accessed in natural order. Which interface provides that capability?",
+        option:[
+            "1.java.util.Map",
+            "2.java.util.Set",
+            "3.java.util.List",
+            "4.	java.util.Collection",   
+        ],
+        answer:2,
+    },
+
+    {
+        question:	
+        	
+        "Which is valid declaration of a float?",
+        option:[
+            "1.float f = 1F;",
+            "2.float f = 1.0;",
+            "3.float f = 1 ; ",
+            "4.	float f = 1.0d;",   
+        ],
+        answer:1,
+    },
+
+    {
+        question:	
+        "What is the numerical range of char?",
+        option:[
+            "1. 0 to 32767",
+            "2. 0 to 65535",
+            "3. -256 to 255",
+            "4. -32768 to 32767",
+        ],
+        answer:2,
+    },
+
+    {
+        question:	
+    	
+        "Which is true about an anonymous inner class?",
+        option:[
+            "1. It can extend exactly one class and implement exactly one interface.",
+            "2. It can extend exactly one class and can implement multiple interfaces.",
+            "3. It can extend exactly one class or implement exactly one interface.",
+            "4. It can implement multiple interfaces regardless of whether it also extends a class.",
+        ],
+        answer:3,
+    },
+]
